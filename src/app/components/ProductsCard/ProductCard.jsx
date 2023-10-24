77d99811264878672f125169101b4b8006e63b3e
GwerhDev <gwerh.dev@gmail.com>
2023-10-24 13:07:23 -0300

    structuring product cards

---
+++ src/app/components/ProductsCard/ProductCard.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import s from './ProductCard.module.css';
+
+export const ProductCard = (props) => {
+  const { id, name, price, image, description, category, rating } = props ? props : null;
+  const navigate = useNavigate();
+
+  function handleClick(e) {
+    e.preventDefault();
+    navigate(`/product/${id}`);
+  }
+
+  return (
+    <div className={s.cardContainer}>
+      <div className={s.imageContainer}>
+        <img src={image} alt="product image" width="100%"/>
+      </div>
+      <h3>{price}</h3>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>{category}</p>
+      <p>{"⭐".repeat(rating)}</p>
+      <button onClick={handleClick}>Ver más</button>
+    </div>
+  )
+}
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,7 +1,17 @@
+import { ProductCard } from "../components/ProductsCard/ProductCard";
+
 const Home = () => {
   return (
     <div className="page-container">
-      
+      <ProductCard 
+        name='Test product' 
+        id='1263128912893120' 
+        price='$1.000.000' 
+        image='' 
+        description='Amplifier test' 
+        category='amps' 
+        rating={5}
+      />
     </div>
   )
 }

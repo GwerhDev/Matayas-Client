90226aec586d5f132e594cfec9d7c4dfe977d4ef
GwerhDev <gwerh.dev@gmail.com>
2023-10-27 13:02:40 -0300

    structuring dashboard anda users/products management

--- src/app/components/ProductCreate/ProductCreate.jsx
+++ src/app/components/ProductCreate/ProductCreate.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import s from './ProductCreate.module.css';
 import { Link } from 'react-router-dom';
 import defaultImage from '../../../assets/png/default-image.png';
 
 export const ProductCreate = () => {
-  function handleSubmit(e) {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [featuredImage, setFeaturedImage] = useState(null);
+
+  function handleGallery(e) {
     e.preventDefault();
     return;
   }
 
-  function handleGallery(e) {
+  function handleSubmit(e) {
     e.preventDefault();
+    const formData = {
+      image: featuredImage,
+      title,
+      price,
+      description,
+    }
+
+    console.log(formData);
     return;
   }
 
@@ -22,20 +36,33 @@ export const ProductCreate = () => {
       <form className="auth-form">
         <span className={s.formImage}>
           <label htmlFor="Image">Imagen principal</label>
-          <img src={defaultImage} alt="" />
-          <input type="file"/>
+          <img src={featuredImage || defaultImage} alt="" />
+          <input
+            type="file"
+            style={{ cursor: 'pointer' }}
+            name="imageSlider"
+            accept="image/jpeg"
+            onChange={(e) => {
+              const file = e.target.files[0];
+              const reader = new FileReader();
+              reader.onloadend = () => {
+                setFeaturedImage(reader.result);
+              }
+              reader.readAsDataURL(file);
+            }}
+          />
         </span>
         <span className={s.formSpan}>
           <label htmlFor="Title">Título</label>
-          <input type="text" placeholder='Ej: sexo, drogas, rock n roll, etc.' />
+          <input onInput={(e) => setTitle(e.target.value)} type="text" placeholder='Ej: sexo, drogas, rock n roll, etc.' />
         </span>
         <span className={s.formSpan}>
           <label htmlFor="Price">Precio</label>
-          <input type="text" placeholder='Ej: $1.000 (CLP)' />
+          <input onInput={(e) => setPrice(e.target.value)} type="text" placeholder='Ej: $1.000 (CLP)' />
         </span>
         <span className={s.formTextarea}>
           <label htmlFor="Description">Descripción</label>
-          <textarea placeholder='Ej: En estricto rigor...' />
+          <textarea onInput={(e) => setDescription(e.target.value)} placeholder='Ej: En estricto rigor...' />
         </span>
         <span className={s.formGallery}>
           <label htmlFor="Gallery">Galería</label>
@@ -45,7 +72,7 @@ export const ProductCreate = () => {
             </button>
           </div>
         </span>
-        <div className='divider'/>
+        <div className='divider' />
         <button onClick={handleSubmit}>Publicar</button>
       </form>
     </div>
--- src/middlewares/redux/actions/admin.js
+++ src/middlewares/redux/actions/admin.js
@@ -1,14 +1,98 @@
 import axios from "axios";
 import { options } from "../../helpers";
 import { URL_API } from "../../config";
-import { GET_USERS } from "../../misc/consts";
+import { CREATE_PRODUCT, CREATE_USER, DELETE_PRODUCT, DELETE_USER, GET_USERS, UPDATE_PRODUCT, UPDATE_USER } from "../../misc/consts";
 
 export function getUsers() {
   return async function (dispatch) {
     try {
-      const response = await axios.get(`${URL_API}/admin/management-users`, options());  
+      const response = await axios.get(`${URL_API}/admin/management-users`, options());
       dispatch({
         type: GET_USERS,
+        payload: response.data
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+export function createUser() {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(`${URL_API}/admin/management-users/create`, options());
+      dispatch({
+        type: CREATE_USER,
+        payload: response.data
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+export function updateUser() {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(`${URL_API}/admin/management-users/update`, options());
+      dispatch({
+        type: UPDATE_USER,
+        payload: response.data
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+export function deleteUser() {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(`${URL_API}/admin/management-users/delete`, options());
+      dispatch({
+        type: DELETE_USER,
+        payload: response.data
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+export function createProduct() {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(`${URL_API}/admin/management-products/create`, options());
+      dispatch({
+        type: CREATE_PRODUCT,
+        payload: response.data
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+export function updateProduct() {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(`${URL_API}/admin/management-products/update`, options());
+      dispatch({
+        type: UPDATE_PRODUCT,
+        payload: response.data
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+export function deleteProduct() {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(`${URL_API}/admin/management-products/delete`, options());
+      dispatch({
+        type: DELETE_PRODUCT,
         payload: response.data
       });
     } catch (e) {

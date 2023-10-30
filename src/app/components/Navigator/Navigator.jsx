6e3296a7584f88ccafac36721f952423d95d3c44
GwerhDev <gwerh.dev@gmail.com>
2023-10-30 18:17:52 -0300

    styling

--- src/app/components/Navigator/Navigator.jsx
+++ src/app/components/Navigator/Navigator.jsx
@@ -52,6 +52,10 @@ export const Navigator = () => {
             <ul>
               <Link to="/"><li>Inicio</li></Link>
               <Link to="/store"><li>Inventario</li></Link>
+              <select name="category" className="select-group nav-select" >
+                <option defaultValue> Categorías</option>
+                <option value={"1"}> Amplificadores</option>
+              </select>
               <Link to="/gallery"><li>Galería de Fotos</li></Link>
               <Link to="/location"><li>Ubicación</li></Link>
               <Link to="/contact"><li>Contacto</li></Link>

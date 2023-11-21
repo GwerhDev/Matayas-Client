3c5696e171b0dc79c7197ee94b659438873ee1ed
GwerhDev <gwerh.dev@gmail.com>
2023-11-21 14:11:31 -0300

    email verification integrated

--- src/app/pages/PendingMailVerification.jsx
+++ src/app/pages/PendingMailVerification.jsx
@@ -1,6 +1,12 @@
 const PendingMailVerification = () => {
   return (
-    <div>PendingVerify</div>
+    <div className="auth-container">
+      <form className="auth-form">
+        <span className="auth-form-text">
+          Te hemos enviado un correo electrónico. Verifica tu bandeja de entrada y sigue las instrucciones para activar tu cuenta.
+        </span>
+      </form>
+    </div>
   )
 }
 

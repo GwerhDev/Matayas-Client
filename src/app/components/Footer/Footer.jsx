b4afa549092389adbf3353cadc1686e86a79db78
GwerhDev <gwerh.dev@gmail.com>
2024-02-13 10:01:01 -0300

    styling

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import s from './Footer.module.css'
+import burgerIcon from '../../../assets/png/matayas-logo.png';
+
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <div className={s.container}>
+      <img src={burgerIcon} alt="" width="80" />
       © { currentYear } Amplificadores Matayas
     </div>
   )

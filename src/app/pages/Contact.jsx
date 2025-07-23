53609e323a285e38daf8ed708fdfa783e4c9b9b4
GwerhDev <gwerh.dev@gmail.com>
2025-07-22 22:58:12 -0400

    updating address

--- src/app/pages/Contact.jsx
+++ src/app/pages/Contact.jsx
@@ -13,7 +13,7 @@ import { Link } from 'react-router-dom';
 const Contact = () => {
   useEffect(() => {
     scrollToTop();
-    }
+  }
   )
 
   return (
@@ -48,14 +48,14 @@ const Contact = () => {
         <section className="map-contact">
           <iframe
             title="Map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.6086914942625!2d-70.64351702315288!3d-33.459499997977694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c56c6b103a01%3A0x6889183662fc2d5b!2sVentura%20Lavalle%20608%2C%208360125%20Santiago%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses!2scl!4v1699024352468!5m2!1ses!2scl"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.8821315569373!2d-70.63949632406886!3d-33.45218649528706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c59a2b5df899%3A0xdf0932446ff67046!2sSantiago%20Concha%201290%2C%208355028%20Santiago%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1721678221671!5m2!1ses-419!2scl"
             width="600"
             height="450"
             style={{ border: 0 }}
             allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          />
         </section>
       </div>
     </div>
